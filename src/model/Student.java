92444cc29f0dcf6f063f3497b366f37a70ca888b
BernalCamilo <jc_bernal2011@hotmail.com>
2024-03-18 16:06:27 -0500

    Feat
    .

--- src/model/Student.java
+++ src/model/Student.java
@@ -2,11 +2,13 @@ package model;
 
 import java.util.concurrent.Semaphore;
 
-class Student extends Thread {
+
+public class Student extends Thread {
     private final Assistant assistant;
     private final Semaphore semaphore;
 
-    public Student(Assistant assistant, Semaphore semaphore) {
+    public Student(String name, Assistant assistant, Semaphore semaphore) {
+        super(name);
         this.assistant = assistant;
         this.semaphore = semaphore;
     }
@@ -16,12 +18,28 @@ class Student extends Thread {
             try {
                 Thread.sleep(1000); // Simulate some waiting time between requests
                 semaphore.acquire(); // Signal the assistant
-                assistant.interrupt(); // Wake up the assistant if it's sleeping
-                Thread.sleep(1000); // Simulate some work after accessing the assistant
-                semaphore.release(); // Release the assistant for others to use
+                if (assistant.isSleeping()) {
+                    System.out.println(getName() + " is waking up the assistant.");
+
+                    assistant.interrupt(); // Wake up the assistant if it's sleeping
+                    System.out.println(getName() + " prueba");
+                } else if (assistant.getWaitlist().size()< 3) {
+                    assistant.addToWaitlist(this);
+                    System.out.println(getName() + " is waiting in line.");
+                    semaphore.release(); // Release the assistant for others to use
+                    Thread.sleep(Long.MAX_VALUE); // Sleep until attended
+                } else {
+                    System.out.println(getName() + " is coding.");
+                    semaphore.release(); // Release the assistant for others to use
+                    Thread.sleep((long) (Math.random() * 5000)); // Simulate coding time
+                }
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    public void waitForAssistance() {
+        interrupt();
     }
 }
